a8da40db22d4c24095b9c82c0ab873e23f1d380e
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-10-19 10:16:05 +0000

    commit

--- async-await-promise-patterns.js
+++ async-await-promise-patterns.js
@@ -1,85 +1,85 @@
-//using promises
-const {readFile,writeFile} = require('fs')
+// //using promises
+// const {readFile,writeFile} = require('fs')
 
-const getFile = (path)=>{
-    return new Promise((resolve,reject)=>{
+// const getFile = (path)=>{
+//     return new Promise((resolve,reject)=>{
 
-        readFile(path,'utf-8', (err, data)=>{
+//         readFile(path,'utf-8', (err, data)=>{
             
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve(data)
-            }
-        })
-        writeFile(path,'utf-8', (err, data)=>{
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve(data)
-            }
-        })
-    })
-}
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//         writeFile(path,'utf-8', (err, data)=>{
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 
-getFile('./myfile.txt','utf-8').then((result)=>{ console.log(result)}).catch((err)=>{console.log(err)})
-const {readFile,writeFile} = require('fs')
+// getFile('./myfile.txt','utf-8').then((result)=>{ console.log(result)}).catch((err)=>{console.log(err)})
+// const {readFile,writeFile} = require('fs')
 
-//using async-await
-const getFile = (path)=>{
-    return new Promise((resolve,reject)=>{
+// //using async-await
+// const getFile = (path)=>{
+//     return new Promise((resolve,reject)=>{
 
-        readFile(path,'utf-8', (err, data)=>{
+//         readFile(path,'utf-8', (err, data)=>{
             
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve(data)
-            }
-        })
-        writeFile(path,'utf-8', (err, data)=>{
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve(data)
-            }
-        })
-    })
-}
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//         writeFile(path,'utf-8', (err, data)=>{
+//             if(err){
+//                 reject(err)
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 
-const start = async ()=>{
-    try{
-        const first = await getFile('./myfile.txt','utf-8')
-        const second = await getFile('./second-myfile.txt',`Here is the result : ${first}`)
-        console.log(first, second)
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+// const start = async ()=>{
+//     try{
+//         const first = await getFile('./myfile.txt','utf-8')
+//         const second = await getFile('./second-myfile.txt',`Here is the result : ${first}`)
+//         console.log(first, second)
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
 
-start()
-//using util module
+// start()
+// //using util module
 
-const util = require('util')
-const {readFile,writeFile} = require('fs')
+// const util = require('util')
+// const {readFile,writeFile} = require('fs')
 
-const readFilePath = util.promisify(readFile)
-const writeFilePath = util.promisify(writeFile)
+// const readFilePath = util.promisify(readFile)
+// const writeFilePath = util.promisify(writeFile)
 
-const start = async ()=>{
-    try{
-        const first = await readFilePath('./myfile.txt','utf-8')
-        await writeFilePath('some-new-file.txt','utf-8',`here is the result ${first}`)
-        console.log(first)
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+// const start = async ()=>{
+//     try{
+//         const first = await readFilePath('./myfile.txt','utf-8')
+//         await writeFilePath('some-new-file.txt','utf-8',`here is the result ${first}`)
+//         console.log(first)
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
 
-start()
+// start()
