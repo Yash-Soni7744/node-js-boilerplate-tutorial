45d38e378680d3560649e835aa98f4ef6d9494dc
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-10-20 03:22:20 +0000

    commit

--- async.js
+++ async.js
@@ -1,10 +1,25 @@
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    console.log('request event')
-    res.end('hello')
-})
+//ctrl + c for exiting the port
+// const http = require('http')
+// const server = http.createServer((req,res)=>{
+//     console.log('request event')
+//     res.end('hello')
+// })
 
-server.listen(5000,()=>{
+// server.listen(5000,()=>{
+//     console.log('Server listening on port : 5000...')
+// })
+
+//using events
+const http = require('http')
+const server = http.createServer()
+
+server.on('error',(error)=>{
+    console.log(error)
+})
+server.on('response', (req, res) => {
+    console.log('Done')
+    res.end('Welcome')
+})
+server.listen(5000, () => {
     console.log('Server listening on port : 5000...')
 })
-//ctrl + c for exiting the port
